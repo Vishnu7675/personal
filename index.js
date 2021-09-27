22c0f30df8ba201f01c89dbf8bb59d8706809a1b
Vishnu7675 <50165721+Vishnu7675@users.noreply.github.com>
2021-09-27 04:59:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function validate(){
      user=document.getElementById("name").value;
      email=document.getElementById("email").value;
+     var subject=document.getElementById("subject").value;
+     var message=document.getElementById("message").value;
      var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
      var nameformat = /^[A-Za-z ]+$/;
      if(user=="")
@@ -31,6 +33,14 @@ function validate(){
             alert("Invalid email format");
         }
 
+     }
+     if(subject=="")
+     {
+         alert("subject can't be blank");
+     }
+     if(message=="")
+     {
+         alert("message can't be blank");
      }
      
      
